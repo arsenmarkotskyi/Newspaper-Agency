d06f3766c5c9458a612a3f2f5a0cf95c4dd38c69
Arsen Markotskyi <arsen.markotskui@gmail.com>
2024-10-29 23:32:48 +0200

    "add models to admin.py"

--- newspaper/admin.py
+++ newspaper/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from newspaper.models import Topic, Newspaper, Redactor
+from django.contrib.auth.models import Group
+
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+
+@admin.register(Newspaper)
+class NewspaperAdmin(admin.ModelAdmin):
+    list_display = ("title", "published_date")
+    list_filter = ("published_date", "topics")
+    search_fields = ("title", "content")
+
+
+@admin.register(Redactor)
+class RedactorAdmin(admin.ModelAdmin):
+    list_display = ("username", "years_of_experience", "email")
+    search_fields = ("username", "email")
+
+
+admin.site.unregister(Group)
