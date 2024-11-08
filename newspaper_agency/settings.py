b5f2e8621546a4ac3e400d3421f28e35b802363d
Arsen Markotskyi <arsen.markotskui@gmail.com>
2024-11-08 22:38:50 +0200

    "add bootstrap, login_url, AUTH_USER_MODEL"

--- newspaper_agency/settings.py
+++ newspaper_agency/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "newspaper",
+    "crispy_forms",
+    "crispy_bootstrap4",
 ]
 
 MIDDLEWARE = [
@@ -122,3 +125,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'newspaper.Redactor'
+
+LOGIN_REDIRECT_URL = "/"
+
+LOGIN_URL = "login"
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
