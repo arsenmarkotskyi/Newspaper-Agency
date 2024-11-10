2343a17af39204ee72ddf8de9819addaf543a8dc
Arsen Markotskyi <arsen.markotskui@gmail.com>
2024-11-10 16:02:40 +0200

    "add ordering by published_date in Newspaper"

--- newspaper/models.py
+++ newspaper/models.py
@@ -16,6 +16,8 @@ class Newspaper(models.Model):
     topics = models.ManyToManyField("Topic", related_name="newspaper")
     publishers = models.ManyToManyField("Redactor", related_name="newspaper")
 
+    class Meta:
+         ordering = ['-published_date']
     def __str__(self):
         return self.title
 
